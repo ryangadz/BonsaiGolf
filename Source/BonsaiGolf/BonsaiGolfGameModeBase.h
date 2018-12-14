08e9c98ee8a4b808cba2a116602182a25611a98d
ryangadz@msn.com <ryangadz@msn.com>
2018-12-13 19:17:30 -0800

    initial project files

---
+++ Source/BonsaiGolf/BonsaiGolfGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BonsaiGolfGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BONSAIGOLF_API ABonsaiGolfGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
